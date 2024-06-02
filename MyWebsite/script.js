1a81c5285b4c52c048be9e0253a5db882331c5b8
0wlish <tortugavpn@gmail.com>
2024-06-01 23:05:57 -0400

    added files

---
+++ MyWebsite/script.js
@@ -0,0 +1,9 @@
+const image = document.querySelector("#rotate");
+const button = document.querySelector("#button");
+
+button.addEventListener("click", () => {
+    image.style.transform = "rotate(90deg)";
+    image.style.transform = "rotate(90deg)";
+    image.style.transform = "rotate(90deg)";
+    image.style.transform = "rotate(90deg)";
+});
